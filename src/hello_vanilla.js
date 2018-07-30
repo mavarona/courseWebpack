5b448f3246a2fcc8835d22a731d953e8b86824ab
mavl <mvaronamac@gmail.coml>
2018-07-30 18:27:43 +0200

    Add two poinst entry and output

---
+++ src/hello_vanilla.js
@@ -0,0 +1,16 @@
+import style from './style.css';
+import sass from './scss/main.scss';
+import data from './data.json';
+import logo from './img/js-logo.png';
+
+const arr = [1, 2, 3],
+    codeES6 = () => console.log(...arr),
+    d = document,
+    app = d.getElementById('app'),
+    img = d.createElement('img');
+
+img.src = logo;
+d.body.appendChild(img);
+
+codeES6()
+console.log(data, data.name);
--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,13 @@ const HtmlWebPackPlugin = require('html-webpack-plugin'),
     CleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = {
-    // entry: {},
-    // output: {},
+    entry: {
+        js: './src/index.js',
+        vanilla: './src/hello_vanilla.js',
+    },
+    output: {
+        filename: '[name].[chunkhash].js'
+    },
     module: {
         rules: [{
             test: /\.js$/,
@@ -26,13 +31,15 @@ module.exports = {
                 'style-loader',
                 MiniCssExtractPlugin.loader,
                 'css-loader?minimize&sourceMap',
+                'postcss-loader?sourceMap',
                 'resolve-url-loader',
                 'sass-loader?outputStyle=compressed&sourceMap'
             ]
         }, {
             test: /\.(jpe?g|png|gif|svg|webp)$/i,
             use: [
-                'file-loader?name=assets/[name].[ext]'
+                'file-loader?name=assets/[name].[ext]',
+                'image-webpack-loader?bypassOnDebug'
             ]
         }, {
             test: /\.(ttf|eot|woff2?|mp4|mp3|txt|xml|pdf)$/i,
@@ -42,7 +49,15 @@ module.exports = {
     plugins: [
         new HtmlWebPackPlugin({
             template: './src/template.html',
-            filename: './index.html'
+            filename: './index.html',
+            hash: true,
+            chunks: ['js']
+        }),
+        new HtmlWebPackPlugin({
+            template: './src/template.html',
+            filename: './hello-vanilla.html',
+            hash: true,
+            chunks: ['vanilla']
         }),
         new MiniCssExtractPlugin({
             filename: '[name].css',
