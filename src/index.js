ee4f6422f88fba1001b9ceb5f6c1a85f064dce73
mavl <mvaronamac@gmail.coml>
2018-07-31 12:17:57 +0200

    change styles and index.html

--- src/index.js
+++ src/index.js
@@ -4,13 +4,29 @@ import data from './data.json';
 import logo from './img/webpack-logo.svg';
 
 const arr = [1, 2, 3],
-    codeES6 = () => console.log(...arr),
-    d = document,
+    codeES6 = () => console.log(...arr);
+
+const d = document,
     app = d.getElementById('app'),
-    img = d.createElement('img');
+    h1 = d.createElement('h1'),
+    p = d.createElement('p'),
+    img = d.createElement('img'),
+    nav = d.createElement('nav');
+
+let menu = '';
+
+data.links.forEach(link => menu += `<a href="${link[1]}">${link[0]}</a>`);
+
+h1.textContent = 'Webpack';
+p.textContent = 'Create first app with webpack';
 
 img.src = logo;
-d.body.appendChild(img);
+nav.classList.add('Menu');
+nav.innerHTML = menu;
+
+app.appendChild(h1);
+app.appendChild(p);
+app.appendChild(nav);
+app.appendChild(img);
 
 codeES6()
-console.log(data, data.name);
